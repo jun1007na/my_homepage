270260af3d361f49c46d041985d6cc3e3ce32be5
Jun@@@&@ <junya.n.107+github@gmail.com>
2017-09-16 09:50:50 +0900

    renewal and repair my homepage

---
+++ js/main.js
@@ -0,0 +1,15 @@
+$(function () {
+    $("#navbar").load("./navbar.html");
+    $("#footer").load("./footer.html");
+
+    $('#maximage').maximage({
+        cycleOptions: {
+            timeout: 5000
+        }
+    });
+
+    $('#c_works').hide();
+    $('#c_ourinfo').fadeIn(500);
+    $('#bg_mosaic').css('background', 'url("/img/bg_mosaic2.png"), url("/img/bg_mosaic3.png")');
+
+});
